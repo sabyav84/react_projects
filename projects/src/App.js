2ee17570b1d518f6bb8fc728d770190016040bc2
sabyav84 <syavasca@gmail.com>
2022-01-06 12:35:33 -0500

    first commit

---
+++ projects/src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+function App() {
+  return (
+    <div>
+      <Container className='d-flex justify-content-center mt-3'>
+        <h1>Wellcome to starter</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
